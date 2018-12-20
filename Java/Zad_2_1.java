848b48f18ad80b99a154dce8113b96a800cf8da1
D-Flime <33832158+D-Flime@users.noreply.github.com>
2018-12-20 20:29:21 +0400

    Update Zad_2_1.java

--- Java/Zad_2_1.java
+++ Java/Zad_2_1.java
@@ -12,16 +12,29 @@ public class Main {
         Scanner in = new Scanner(System.in);
         Random random = new Random();
 
-        int n, k;
+        int n;
+        int maxi = 0;
+        int maxj = 0;
+        double k;
+        double max = 0;
         System.out.print("Длина массива = "); n = in.nextInt();
         double[] arr = new double[n];
         for (byte i=0; i < n; i++){
-            double min = -10;
-            double max = 200;
-            double diff = max - min;
-            k = random.nextDouble(diff + 1);
-            k += min;
+            k = -10 + random.nextDouble()*20;
+            System.out.println(k);
             arr[i] = k;
         }
+        for (int i = 0; i < n; i++){
+            for (int j = i+1; j < n; j++){
+                if (max < arr[i]+arr[j]){
+                    max = arr[i]+arr[j];
+                    maxi = i;
+                    maxj = j;
+                }
+            }
+        }
+        System.out.println("Индексы элементов массива с максимальной суммой:");
+        System.out.println(maxi);
+        System.out.println(maxj);
     }
 }
