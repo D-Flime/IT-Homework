32923ed4a5367631529f742fba301eab27aab857
D-Flime <33832158+D-Flime@users.noreply.github.com>
2019-01-04 20:37:21 +0300

    Create Zad_2_2.java

---
+++ Java/Zad_2_2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        
+        int i1, j1;
+        i1 = 0; j1 = 0;
+        final int n = 3;
+        int[][] arr = new int[n][n];
+        int[][] arr1 = new int[n][n];
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                arr[i][j] = ThreadLocalRandom.current().nextInt(0, 9);
+                System.out.print(arr[i][j]);
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+        System.out.println(); System.out.println();
+        for (int i = n-1; i >= 0; i--){
+            for (int j = n-1; j >= 0; j--){
+                arr1[i1][j1] = arr[i][j];
+                System.out.print(arr[i][j]);
+                System.out.print("  ");
+                j1++;
+            }
+            i1++;
+            j1 = 0;
+            System.out.println();
+        }
+    }
+}
